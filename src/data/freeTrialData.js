8a1e8341b0d926a5e41a10788ef1c58210287577
YURULIN1995 <xd22kkb22likex@gmail.com>
2025-06-19 00:52:16 +0800

    Complete Navbar and Hero RWD components.

--- src/data/freeTrialData.js
+++ src/data/freeTrialData.js
@@ -4,7 +4,7 @@ export const freeTrialData  = {
   titleLine1: "千人好評推薦！每日限量",
   titleLine2: "職人抹茶雙品 免費試吃",
   paragraph: "嚴選京都宇治一番茶，石臼慢磨，香氣細緻。搭配濃厚抹茶手工餅乾，雙重風味一次滿足。立即索取，品味真正的日式講究。",
-  buttonText: "立即免費索取",
+  buttonText: "立即免費索取 ⇒",
   buttonLink: "#", 
   socialLinks: [
     { id: 'fb', name: 'Facebook', link: '#', iconClass: 'fab fa-facebook' },
