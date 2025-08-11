4bb83d0ecf0e1eb1703805c678e033c4c2f319d4
YURULIN1995 <xd22kkb22likex@gmail.com>
2025-08-11 17:42:14 +0800

    Adjust line-height parameter both in _mixins.scss and in PostCard.module.scss.

--- src/data/testimonialsData.js
+++ src/data/testimonialsData.js
@@ -8,7 +8,7 @@ export const testimonials = [
     // 【新增】用於首頁「客戶案例分享」的短引言
     highlight: "Lorem ipsum dolor sit amet",
     // 將原來的 text 改名為 fullText，用於「關於我」頁面的完整心得
-    fullText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam in pulvinar neque. Phasellus viverra dolor odio, tempor sollicitudin metus laoreet sit amet.",
+    fullText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam in pulvinar neque. \n\n Phasellus viverra dolor odio, tempor sollicitudin metus laoreet sit amet.",
     // 【新增】點擊後可前往的連結，可以是作品集或更詳細的文章 (可選)
     link: "#" 
   },
@@ -19,7 +19,7 @@ export const testimonials = [
     imageUrl: "https://placehold.co/150x150/a9d3a4/4a6e4f?text=Avatar",
     imageAlt: "B-avatar",
     highlight: "Lorem ipsum dolor sit amet",
-    fullText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam in pulvinar neque. Phasellus viverra dolor odio, tempor sollicitudin metus laoreet sit amet.",
+    fullText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam in pulvinar neque. \n\n Phasellus viverra dolor odio, tempor sollicitudin metus laoreet sit amet.",
     link: "#"
   },
   {
@@ -29,7 +29,7 @@ export const testimonials = [
     imageUrl: "https://placehold.co/150x150/a9d3a4/4a6e4f?text=Avatar",
     imageAlt: "C-avatar",
     highlight: "Lorem ipsum dolor sit amet",
-    fullText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam in pulvinar neque. Phasellus viverra dolor odio, tempor sollicitudin metus laoreet sit amet.",
+    fullText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam in pulvinar neque. \n\n Phasellus viverra dolor odio, tempor sollicitudin metus laoreet sit amet.",
     link: "#"
   }
 ];
