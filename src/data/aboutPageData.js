1e94abe3121d09e74205f0c5e263944d33772486
YURULIN1995 <xd22kkb22likex@gmail.com>
2025-08-11 17:55:55 +0800

    Modify padding in .leftWrap and .rightWrap in PostCard.module.scss.

--- src/data/aboutPageData.js
+++ src/data/aboutPageData.js
@@ -3,7 +3,7 @@ import { testimonials } from '@data/testimonialsData.js';
 export const authorIntroData = {
   titleLine1: "關於我",
   paragraph: "我是一隻雲林快樂豬！🐷🐷🐷🐷🐷🐷🐷🐷🐷🐷🐷🐷🐷🐷🐷🐷🐷🐷🐷🐷🐷🐷🐷🐷🐷🐷🐷🐷🐷🐷🐷🐷🐷🐷🐷🐷🐷🐷🐷🐷🐷🐷🐷🐷🐷🐷🐷🐷🐷🐷🐷🐷🐷🐷🐷🐷🐷🐷🐷🐷🐷🐷🐷🐷🐷🐷🐷🐷🐷🐷🐷🐷🐷🐷🐷🐷🐷🐷🐷🐷🐷🐷🐷🐷🐷🐷🐷🐷",
-  buttonText: "🐷🐷成果",
+  buttonText: "客戶成果",
   buttonLink: "/portfolio",
   imageUrl: "https://placehold.co/500x500/a9d3a4/4a6e4f?text=Author",
   imageAlt: "Author profile placeholder image",
