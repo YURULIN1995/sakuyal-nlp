7d5f81cc89b2aa4f17f4c8d7245d95acbf36362b
YURULIN1995 <xd22kkb22likex@gmail.com>
2025-08-15 12:29:32 +0800

    Strech BackgroundColo to full-height..

--- src/pages/content/ContactPage.jsx
+++ src/pages/content/ContactPage.jsx
@@ -2,21 +2,20 @@ import SEO from '@components/Head/SEO';
 import styles from '@styles/ContactPage.module.scss';
 import PageHeaderBicolorBackground from '@components/PageHeaderBicolorBackground';
 import BackgroundColor from '@components/BackgroundColor';
-import Divider from '@components/Divider';
 import { contactData } from '@data/contactPageData.js';
 import ContactLinks from '@components/ContactLinks';
 import ContactForm from '@components/ContactForm';
+import Divider from '@components/Divider';
 
 function ContactPage() {
-  console.log("Rendering ContactPage");
-
   return (
-    <>
+    // 【新增】用 pageWrapper 包裹整個頁面
+    <div className={styles.pageWrapper}>
       <SEO title="聯絡" description="這是 Sakuyal 自然語言煉金術的聯絡頁面。" />
       <PageHeaderBicolorBackground title={contactData.titleLine1}/>
       
-      {/* 原有的介紹區塊 */}
-      <BackgroundColor color="lightGreen">
+      {/* 【修改】傳入新的 className */}
+      <BackgroundColor color="lightGreen" className={styles.growingSection}>
         <div className={styles.contactSection}>
           <div className={styles.contactTextWrap}>
             <h2 className={styles.title}>{contactData.titleLine2}</h2>
@@ -31,10 +30,11 @@ function ContactPage() {
             />
           </div>
         </div>
+        
         <Divider />
         <ContactForm />
       </BackgroundColor>
-    </>
+    </div>
   );
 }
 
